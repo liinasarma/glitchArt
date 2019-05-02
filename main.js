97ee65926f79aaf0ecd24cd376d2bba3e9b7a7a1
unknown <lina.sarma@gmail.com>
2019-05-02 17:54:30 +0300

    img with tiles

--- main.js
+++ main.js
@@ -1,18 +1,51 @@
 let img;
+let img2;
 
 function preload(){
 
-img = loadImage('https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/cc_20120702-1848_16x9.jpg?itok=embMTBY2');
+img = loadImage('https://upload.wikimedia.org/wikipedia/commons/6/69/June_odd-eyed-cat_cropped.jpg');
+img2 = loadImage('https://upload.wikimedia.org/wikipedia/commons/6/69/June_odd-eyed-cat_cropped.jpg');
 }
 
 function setup(){
-createCanvas(1200,800);
-
+createCanvas(1200,800, WEBGL);
+img.loadPixels();
+img2.loadPixels();
 
 }
 
 function draw(){
-background(100);
- image(img, 0, 0);
+background(250);
+
+let col;
+
+push();
+translate(-600,-400);
+image(img2, 0, 0, 600,400);
+
+copy(img2,0,150,img2.width,1,0,150,img2.width,-img2.height-150);
+pop();
+
+
+for(var x = 0; x<600; x+=10){
+for(var y = 0; y<400; y+=10){
+
+console.log(x);
+console.log(y);
+col = img.get(x,y);
+
+fill(col);
+//ellipse(x,y,10,10);
+push();
+translate(x,y);
+box(8);
+pop();
+
+
+}
+}
+
+
+
 
 }
